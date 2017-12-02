444f363bbbe26d6ab81aeafb7425bcd61fb38de1
groovin101 <groovin101@gmail.com>
2017-12-01 16:59:30 -0800

    Initial creation

---
+++ src/main/java/com/groovin101/refresher/App.java
@@ -0,0 +1,13 @@
+package com.groovin101.refresher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
